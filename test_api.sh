eabe88a7a603ba4aff3b6c399f7b258fb6cfce71
klaushofrichter <klaus@klaushofrichter.net>
2025-04-21 18:55:45 -0500

    Enhance test_api.sh to include jq dependency check and improve output formatting
    - Added a check for the jq command to ensure it is installed before running the script.
    - Updated output to format API responses using jq for better readability.
    - Included account_id in the output after sign-in for additional context.

--- test_api.sh
+++ test_api.sh
@@ -2,12 +2,18 @@
 #
 # This script is used to test the API - it performs a CURL sign-in and prints the 
 # result reading the .env for configuration. Then it refershes the token, and finally
-
 #
 
 # 
 # read configuration and secrets
 source .env
+
+#
+# jq is needed
+if ! command -v jq &> /dev/null; then
+  echo "jq could not be found, please install it"
+  exit 1
+fi
 
 #
 # if VERBOSE is set to "-v" then CURL print the verbose output. 
@@ -33,24 +39,26 @@ RESP=$( curl ${VERBOSE} -X POST "${VITE_SWIFT_SENSORS_API_URL}" \
   -d "{\"email\":\"${VITE_SWIFT_SENSORS_USER}\",\
        \"password\":\"${VITE_SWIFT_SENSORS_PASSWORD}\",\
        \"language\": \"en\"}" )
-echo "${RESP}"
+echo "${RESP}" | jq
 ACCESSTOKEN=$( echo "${RESP}" | jq -r '.access_token' )
 REFRESHTOKEN=$( echo "${RESP}" | jq -r '.refresh_token' )
+ACCOUNTID=$( echo "${RESP}" | jq -r '.account_id' )
 echo "access_token: ${ACCESSTOKEN}"  
 echo "refresh_token: ${REFRESHTOKEN}"  
+echo "account_id: ${ACCOUNTID}"
 echo
 sleep 5
 
 #
 # Make the refresh 
-echo "Making the refresh API call: ${VITE_SWIFT_SENSORS_API_URL}"
 VITE_SWIFT_SENSORS_API_URL="${VITE_SWIFT_SENSORS_API_HOST}/api/token/v2/refresh"
+echo "Making the refresh API call: ${VITE_SWIFT_SENSORS_API_URL}"
 RESP=$( curl ${VERBOSE} -X POST "${VITE_SWIFT_SENSORS_API_URL}" \
   -H "X-API-Key: ${VITE_SWIFT_SENSORS_API_KEY}" \
   -H "Content-Type: text/plain" \
   -H "Authorization: Bearer ${ACCESSTOKEN}" \
   -d "${REFRESHTOKEN}" )   
-echo "${RESP}"
+echo "${RESP}" | jq
 ACCESSTOKEN2=$( echo "${RESP}" | jq -r '.access_token' )
 REFRESHTOKEN2=$( echo "${RESP}" | jq -r '.refresh_token' )
 echo "access_token: ${ACCESSTOKEN2}"  
@@ -66,7 +74,7 @@ RESP=$( curl ${VERBOSE} -X POST "${VITE_SWIFT_SENSORS_API_URL}" \
   -H "Content-Type: text/plain" \
   -H "Authorization: Bearer ${ACCESSTOKEN}" \
   -d "${REFRESHTOKEN}" )    # OLD TOKEN, SHOULD NOT WORK ANYMORE
-echo "${RESP}"
+echo "${RESP}" | jq
 ACCESSTOKEN3=$( echo "${RESP}" | jq -r '.access_token' )
 REFRESHTOKEN3=$( echo "${RESP}" | jq -r '.refresh_token' )
 echo "access_token: ${ACCESSTOKEN3}"  
@@ -79,3 +87,15 @@ else
   echo "the refresh token cannot be reused"
 fi
 
+echo
+sleep 5
+
+#
+# Make the treeupdate API call
+VITE_SWIFT_SENSORS_API_URL="${VITE_SWIFT_SENSORS_API_HOST}/api/client/v1/accounts/${ACCOUNTID}/treeupdate"
+echo "Making the treeupdate API call: ${VITE_SWIFT_SENSORS_API_URL}"
+RESP=$( curl ${VERBOSE} -X GET "${VITE_SWIFT_SENSORS_API_URL}" \
+  -H "X-API-Key: ${VITE_SWIFT_SENSORS_API_KEY}" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer ${ACCESSTOKEN}" )
+echo "${RESP}" | jq
