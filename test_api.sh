621d0b16e10bf07b96926204a4221a7aac63e395
klaushofrichter <klaus@klaushofrichter.net>
2025-04-21 18:30:44 -0500

    Update README.md and test_api.sh for improved API interaction
    - Clarified the automatic access token refresh feature in README.md.
    - Modified test_api.sh to include verbose output options and updated API endpoint for sign-in.
    - Enhanced the refresh token handling logic to check if the refresh token can be reused.

--- test_api.sh
+++ test_api.sh
@@ -9,8 +9,13 @@
 source .env
 
 #
+# if VERBOSE is set to "-v" then CURL print the verbose output. 
+# "-s" is silent mode.
+VERBOSE="-s"
+
+#
 # remove the proxy
-VITE_SWIFT_SENSORS_API_URL="${VITE_SWIFT_SENSORS_API_HOST}/${VITE_SWIFT_SENSORS_PROXY_API_URL:5}/v1/sign-in"
+VITE_SWIFT_SENSORS_API_URL="${VITE_SWIFT_SENSORS_API_HOST}/api/client/v1/sign-in"
 
 #
 # show the parameters
@@ -21,7 +26,7 @@ echo "api-key: ${VITE_SWIFT_SENSORS_API_KEY:0:5}..."
 
 #
 # Make the singn-in API call
-RESP=$( curl -v -X POST "${VITE_SWIFT_SENSORS_API_URL}" \
+RESP=$( curl ${VERBOSE} -X POST "${VITE_SWIFT_SENSORS_API_URL}" \
   -H "X-API-Key: ${VITE_SWIFT_SENSORS_API_KEY}" \
   -H "Content-Type: application/json" \
   -d "{\"email\":\"${VITE_SWIFT_SENSORS_USER}\",\
@@ -32,20 +37,44 @@ ACCESSTOKEN=$( echo "${RESP}" | jq -r '.access_token' )
 REFRESHTOKEN=$( echo "${RESP}" | jq -r '.refresh_token' )
 echo "access_token: ${ACCESSTOKEN}"  
 echo "refresh_token: ${REFRESHTOKEN}"  
+echo
 sleep 5
 
 #
 # Make the refresh 
-#VITE_SWIFT_SENSORS_API_URL="${VITE_SWIFT_SENSORS_API_HOST}/api/token/v2/refresh"
-#curl -v -X POST "${VITE_SWIFT_SENSORS_API_URL}" \
-#   -H "X-API-Key: ${VITE_SWIFT_SENSORS_API_KEY}" \
-#  -H "Content-Type: text/plain" \
-#  -d "${REFRESHTOKEN}" 
-
-#
-# Make the API call
-curl -v -X POST https://api.swiftsensors.net/api/token/v2/refresh \
+echo "Making the refresh API call: ${VITE_SWIFT_SENSORS_API_URL}"
+VITE_SWIFT_SENSORS_API_URL="${VITE_SWIFT_SENSORS_API_HOST}/api/token/v2/refresh"
+RESP=$( curl ${VERBOSE} -X POST "${VITE_SWIFT_SENSORS_API_URL}" \
   -H "X-API-Key: ${VITE_SWIFT_SENSORS_API_KEY}" \
   -H "Content-Type: text/plain" \
-  -H "Authorization: Bearer ${ACCESSTOKEN}"  \
-  -d "${REFRESHTOKEN}" 
+  -H "Authorization: Bearer ${ACCESSTOKEN}" \
+  -d "${REFRESHTOKEN}" )   
+echo "${RESP}"
+ACCESSTOKEN2=$( echo "${RESP}" | jq -r '.access_token' )
+REFRESHTOKEN2=$( echo "${RESP}" | jq -r '.refresh_token' )
+echo "access_token: ${ACCESSTOKEN2}"  
+echo "refresh_token: ${REFRESHTOKEN2}"  
+echo
+
+#
+# trying to reuse the old access token
+sleep 5
+echo "Trying to reuse the old refresh token to refresh again"
+RESP=$( curl ${VERBOSE} -X POST "${VITE_SWIFT_SENSORS_API_URL}" \
+  -H "X-API-Key: ${VITE_SWIFT_SENSORS_API_KEY}" \
+  -H "Content-Type: text/plain" \
+  -H "Authorization: Bearer ${ACCESSTOKEN}" \
+  -d "${REFRESHTOKEN}" )    # OLD TOKEN, SHOULD NOT WORK ANYMORE
+echo "${RESP}"
+ACCESSTOKEN3=$( echo "${RESP}" | jq -r '.access_token' )
+REFRESHTOKEN3=$( echo "${RESP}" | jq -r '.refresh_token' )
+echo "access_token: ${ACCESSTOKEN3}"  
+echo "refresh_token: ${REFRESHTOKEN3}"  
+
+echo
+if [ "${ACCESSTOKEN3}" != "" ]; then
+  echo "the refresh token can be reused"
+else
+  echo "the refresh token cannot be reused"
+fi
+
